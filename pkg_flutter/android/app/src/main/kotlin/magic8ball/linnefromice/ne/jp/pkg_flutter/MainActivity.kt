15e73bece1040636d66b315b54109559184e7ae7
linnefromice <linnefromice@gmail.com>
2020-01-07 20:39:27 +0900

    first commit

---
+++ pkg_flutter/android/app/src/main/kotlin/magic8ball/linnefromice/ne/jp/pkg_flutter/MainActivity.kt
@@ -0,0 +1,12 @@
+package magic8ball.linnefromice.ne.jp.pkg_flutter
+
+import android.support.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
