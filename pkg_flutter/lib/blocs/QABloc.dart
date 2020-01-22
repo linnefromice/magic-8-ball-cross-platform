45e64ae8a890ab26c0609745455a5a904e1b55fe
linnefromice <linnefromice@gmail.com>
2020-01-23 00:34:45 +0900

    add bloc (no use)

---
+++ pkg_flutter/lib/blocs/QABloc.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:pkg_flutter/services/AnswerService.dart';
+
+class QABloc {
+  final _questionController = StreamController<String>();
+  Sink<String> get addQuestion => _questionController.sink;
+
+  final _answerController = StreamController<String>();
+  Stream<String> get getAnswer => _answerController.stream;
+
+  QABloc() {
+    _replyRandomAnswer();
+  }
+
+  void _replyRandomAnswer() {
+    _questionController.stream.listen((_) {
+      var selectedAnswer = AnswerService.selectRandomAnswer();
+      _answerController.add(selectedAnswer);
+    });
+  }
+
+  void dispose() {
+    _questionController.close();
+    _answerController.close();
+  }
+}
