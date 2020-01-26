22f2aa807cbdf56361f51a0dfa442bda98bc38c6
linnefromice <linnefromice@gmail.com>
2020-01-26 12:46:45 +0900

    modify title

--- pkg_flutter_bloc/lib/main.dart
+++ pkg_flutter_bloc/lib/main.dart
@@ -11,7 +11,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Magic 8 Ball with Flutter / BLoC architecture',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
